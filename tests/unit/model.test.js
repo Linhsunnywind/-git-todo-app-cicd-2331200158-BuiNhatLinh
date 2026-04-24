9bfb3daaaea87bca2ee7fd3109d9159ee814fdfb
Bui Nhat Linh <buinhatlinh2505@gmail.com>
2026-04-24 08:40:07 +0700

    Test: Implement unit tests for TodoService

--- tests/unit/model.test.js
+++ tests/unit/model.test.js
@@ -10,27 +10,52 @@ describe('TodoService Unit Tests', () => {
         service.todos = [];
     });
 
+    function sum(a, b) {
+        return a + b;
+    }
+
     test('should add a new todo', () => {
         // TODO: Call the addTodo method with some text.
         // Then, assert that the service's todos array has a length of 1.
         // Assert that the text of the first todo matches the input text.
+        text = "Some text"
+        service.addTodo("Some text");
+
+        expect(service.todos.length).toBe(1);
+        expect(service.todos[0].text).toMatch(text);
+
     });
 
     test('should toggle the completed state of a todo', () => {
         // TODO: First, add a todo.
+        service.addTodo("Task 1");
         // Then, get its ID and call the toggleTodoComplete method.
+        const id = service.todos[0].id;
         // Assert that the 'completed' property of that todo is now true.
+        service.toggleTodoComplete(id);
+        expect(service.todos[0].completed).toBe(true);
         // Call toggleTodoComplete again and assert that it's false.
+        service.toggleTodoComplete(id);
+        expect(service.todos[0].completed).toBe(false);
+
     });
 
     test('should remove a todo', () => {
         // TODO: Add a todo.
+        service.addTodo("Task to delete");
         // Get its ID and call the removeTodo method.
+        const id = service.todos[0].id;
         // Assert that the service's todos array is now empty (length of 0).
+        service.removeTodo(id);
+        // Assert that the service's todos array is now empty (length of 0)
+        expect(service.todos.length).toBe(0);
+
     });
 
     test('should not add a todo if text is empty', () => {
         // TODO: Call addTodo with an empty string.
+        service.addTodo("");
         // Assert that the todos array still has a length of 0.
+        expect(service.todos.length).toBe(0);
     });
 });
